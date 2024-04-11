dba6cf183db720c46259723a4c578ff7adef62ca
Victor Mutiso <mutisovictor69@gmail.com>
2024-04-11 20:31:47 +0300

    viCracker

---
+++ packeSniffer.py
@@ -0,0 +1,17 @@
+import socket
+from scapy.all import *
+from scapy.layers.l2 import Ether
+
+sniffer_socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+interface = "eth0"
+
+sniffer_socket.bind((interface, 0))
+
+try:
+    while True:
+        raw_data, addr = sniffer_socket.recvfrom(65535)
+        packet = Ether(raw_data)
+        print(packet.summary())
+except KeyboardInterrupt:
+    sniffer_socket.close()
